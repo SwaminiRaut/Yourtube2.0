7de59ab76cb1b1d90fe1b01cdb0619fd9704a609
SwaminiRaut <rautswamini55@gmail.com>
2025-10-29 22:11:50 +0530

    Update axiosInstance.js

--- yourtube/src/lib/axiosInstance.js
+++ yourtube/src/lib/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://yourtube2-0-9t2o.onrender.com",
   withCredentials: true, // for auth cookies
   headers: {
     "Content-Type": "application/json",
