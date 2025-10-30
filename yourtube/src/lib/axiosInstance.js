5944fb676bce3197c8a0d4564471908f41263e24
SwaminiRaut <rautdswamini55@gmail.com>
2025-10-30 20:02:43 +0530

    Update AxiosInstance

--- yourtube/src/lib/axiosInstance.js
+++ yourtube/src/lib/axiosInstance.js
@@ -1,12 +1,17 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NEXT_PUBLIC_BACKEND_URL ||
+  (process.env.NODE_ENV === "development"
+    ? "http://localhost:5000"
+    : "https://yourtube2-0-9t2o.onrender.com");
+
 const axiosInstance = axios.create({
-  baseURL: "https://yourtube2-0-9t2o.onrender.com",
-  withCredentials: true, // for auth cookies
+  baseURL,
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export default axiosInstance;
-
