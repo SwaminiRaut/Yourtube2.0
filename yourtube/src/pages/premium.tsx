a9d91398acd9bb70aef9657d3c796df4032894ea
SwaminiRaut <rautdswamini55@gmail.com>
2025-10-31 19:52:29 +0530

    Update premium.tsx

--- yourtube/src/pages/premium.tsx
+++ yourtube/src/pages/premium.tsx
@@ -27,7 +27,7 @@ const Premium = () => {
       return;
     }
 
-    const response = await fetch("https://yourtube2-0-9t2o.onrender.com/api/create-order", {
+    const response = await fetch("https://yourtube2-0-9t2o.onrender.com/payment/create-order", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ amount: 199 }),
