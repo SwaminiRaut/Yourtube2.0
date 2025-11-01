da8bd408caf1280f3d27926fd66357c45a1e09f1
SwaminiRaut <rautswamini55@gmail.com>
2025-11-01 23:15:37 +0530

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,7 +3,6 @@ import { activatePlan } from "../controllers/user.js";
 
 const router = express.Router();
 
-// POST /user/activate-plan
 router.post("/activate-plan", activatePlan);
 
 export default router;
