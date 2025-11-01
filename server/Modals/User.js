e217800297a4e66c7894df6e822c0edcafaf4973
SwaminiRaut <rautswamini55@gmail.com>
2025-11-01 23:01:29 +0530

    Update User.js

--- server/Modals/User.js
+++ server/Modals/User.js
@@ -1,65 +1,3 @@
-// import mongoose from "mongoose";
-
-// const userSchema = new mongoose.Schema(
-//   {
-//     name: {
-//       type: String,
-//       required: true,
-//       trim: true,
-//     },
-//     email: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//       lowercase: true,
-//       trim: true,
-//     },
-//     password: {
-//       type: String,
-//       required: true,
-//       minlength: 6,
-//     },
-//     isPremium: {
-//       type: Boolean,
-//       default: false, // ✅ for free users (false means free plan)
-//     },
-//     lastDownloadDate:{
-//       type:Date,
-//       default:null,
-//     },
-//     plan:{
-//       type:String,
-//       enum:["free", "bronze", "silver", "gold"],
-//       default:"free",
-//     },
-//     planExpiry:{
-//       type:Date,
-//       default:null,
-//     },
-//     otp:{
-//       type:String,
-//     },
-//     timezone:{
-//       type:String,
-//     },
-//     otpExpiry:{
-//       type:Date,
-//     },
-//     phonenumber:{
-//       type:String,
-//       required:false,
-//     },
-//   },
-//   { timestamps: true }
-// );
-
-// const User = mongoose.models.User || mongoose.model("User", userSchema);
-
-// export default User;
-
-
-
-
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
@@ -78,7 +16,7 @@ const userSchema = new mongoose.Schema(
     },
     password: {
       type: String,
-      required: false, // optional if using Google login
+      required: false, 
       minlength: 6,
     },
     channelname: {
@@ -128,7 +66,7 @@ const userSchema = new mongoose.Schema(
       type: String,
     },
     phoneNumber: {
-      type: String, // ✅ important for SMS OTP
+      type: String, 
       required: false,
     },
     lastDownloadDate: {
