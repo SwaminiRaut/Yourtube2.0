e115a5756e8847d933e55224751f2d7d571515ee
SwaminiRaut <rautswamini55@gmail.com>
2025-11-01 23:58:26 +0530

    Update call.tsx

--- yourtube/src/pages/call.tsx
+++ yourtube/src/pages/call.tsx
@@ -3,7 +3,6 @@ import VideoCall from "@/components/videoCall";
 export default function CallPage() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      {/* Example: swap these IDs in another tab */}
       <VideoCall currentUserId="user1" targetUserId="user2" />
     </div>
   );
